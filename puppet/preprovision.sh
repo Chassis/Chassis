12e4ad19e2c06b37f57db7cfbf43c2f2f4adf88d
Ryan McCue <me@ryanmccue.info>
2014-03-29 17:41:52 +1000

    Prepend apt mirrors to the file instead of appending

--- puppet/preprovision.sh
+++ puppet/preprovision.sh
@@ -2,10 +2,21 @@ if [[ ! -f /etc/chassis-updated ]]; then
 	MIRROR="mirror://mirrors.ubuntu.com/mirrors.txt"
 	REPOS="main restricted universe multiverse"
 
-	echo "deb $MIRROR precise $REPOS"           >> /etc/apt/sources.list
-	echo "deb $MIRROR precise-updates $REPOS"   >> /etc/apt/sources.list
-	echo "deb $MIRROR precise-backports $REPOS" >> /etc/apt/sources.list
-	echo "deb $MIRROR precise-security $REPOS"  >> /etc/apt/sources.list
+	touch /tmp/mirrors-sources.list
+	echo "deb $MIRROR precise $REPOS"           >> /tmp/mirrors-sources.list
+	echo "deb $MIRROR precise-updates $REPOS"   >> /tmp/mirrors-sources.list
+	echo "deb $MIRROR precise-backports $REPOS" >> /tmp/mirrors-sources.list
+	echo "deb $MIRROR precise-security $REPOS"  >> /tmp/mirrors-sources.list
+
+	# Add mirrors to the start
+	cat /tmp/mirrors-sources.list /etc/apt/sources.list > /tmp/apt-sources.list
+
+	# Move into place
+	cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	mv /tmp/apt-sources.list /etc/apt/sources.list
+
+	# Remove temp files
+	rm /tmp/mirrors-sources.list /tmp/apt-sources.list
 
 	apt-get update
 
