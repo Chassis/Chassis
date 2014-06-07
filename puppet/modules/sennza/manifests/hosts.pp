67f1a929e0ce045faa2936bc516d2c483ee21e3a
Ryan McCue <me@ryanmccue.info>
2014-06-07 16:17:16 +1000

    Add automatic host manager

--- puppet/modules/sennza/manifests/hosts.pp
+++ puppet/modules/sennza/manifests/hosts.pp
@@ -11,6 +11,11 @@ class sennza::hosts(
 		require => Package['python-pip'],
 	}
 
+	file { '/etc/init/chassis-hosts.conf':
+		source => "puppet:///modules/sennza/hosts.init",
+		mode => "0644",
+	}
+
 	file { [ '/etc/chassis-hosts', '/etc/chassis-hosts/conf.d' ]:
 		ensure => directory
 	}
@@ -26,5 +31,17 @@ class sennza::hosts(
 	file { '/etc/chassis-hosts/conf.d/subdomains':
 		ensure => file,
 		owner => "www-data",
+	}
+
+	service { "chassis-hosts":
+		ensure => running,
+		require => [
+			Package[ 'avahi-daemon' ],
+			Package[ 'python-avahi' ],
+			Package[ 'watchdog' ],
+			File[ '/etc/init/chassis-hosts.conf' ],
+			File[ '/etc/chassis-hosts/conf.d/aliases' ],
+			File[ '/etc/chassis-hosts/conf.d/subdomains' ],
+		]
 	}
 }
