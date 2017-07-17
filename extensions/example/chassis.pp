a7d9b8b601849bfa05fede6be466a67ef3ab58e1
Ryan McCue <me@ryanmccue.info>
2017-07-17 14:33:02 +1000

    Add dummy chassis.pp to avoid import warning

---
+++ extensions/example/chassis.pp
@@ -0,0 +1,19 @@
+################################################################################
+#
+#                             EXTENSION DEVELOPERS:
+#
+#  chassis.pp is now deprecated with extensions v2. Extensions should instead
+#                              use Puppet modules:
+#
+#          http://docs.chassis.io/en/latest/extend/#extensions-api-v2
+#
+################################################################################
+#
+#                           CHASSIS CORE DEVELOPERS:
+#
+#  This file is required to ensure the `import extensions/*/chassis.pp` doesn't
+#         fail for fresh checkouts (Puppet requires at least one file).
+#
+#                           Do not remove this file!
+#
+################################################################################
