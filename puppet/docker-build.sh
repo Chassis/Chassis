33af6c06a6644fdf7b37f7188a3a13e50503446e
Ryan McCue <me@ryanmccue.info>
2017-11-15 17:08:48 +1000

    Use nicer syntax for spawning Puppet

--- puppet/docker-build.sh
+++ puppet/docker-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 PID=$$
-bash -c 'puppet apply \
- 	--modulepath "/vagrant/puppet/modules" \
- 	--hiera_config /dev/null \
- 	--disable_warnings=deprecations \
- 	/vagrant/puppet/manifests/docker.pp || sleep 2; \
- 	kill '$PID & exec /sbin/my_init
+(
+	puppet apply \
+		--modulepath "/vagrant/puppet/modules" \
+		--hiera_config /dev/null \
+		--disable_warnings=deprecations \
+		/vagrant/puppet/manifests/docker.pp || sleep 2; \
+ 	kill $PID
+) &
+
+exec /sbin/my_init
