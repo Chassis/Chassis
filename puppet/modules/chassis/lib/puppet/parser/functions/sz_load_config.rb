3075ecc6d8654a6d49d54528f05f314e37551caf
Kaspars Dambis <hi@kaspars.net>
2018-09-23 09:40:31 +0300

    Core from /chassis and config from /vagrant
    Which can be the same in some instances

--- puppet/modules/chassis/lib/puppet/parser/functions/sz_load_config.rb
+++ puppet/modules/chassis/lib/puppet/parser/functions/sz_load_config.rb
@@ -1,4 +1,6 @@
-require "/vagrant/puppet/chassis"
+require "/chassis/puppet/chassis"
+
+Chassis.class_variable_set(:@@config_dir, '/vagrant')
 
 module Puppet::Parser::Functions
 	newfunction(:sz_load_config, :type => :rvalue) do |args|
