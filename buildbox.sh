415bf6746625d955ec469dda4590ff3ac2e20612
Bronson Quick <bronson@bronsonquick.com.au>
2018-10-20 20:40:23 +1100

    Switch to `vagrant cloud` which shipped in 2.2.0

--- buildbox.sh
+++ buildbox.sh
@@ -2,25 +2,8 @@
 # A script to build a new base box for Vagrant Cloud. This needs to be run
 # on the host computer and not inside the virtual machine.
 
-# You need to have https support for curl. If you have Brew you can do that by
-# running the following commands in a terminal:
-# `brew install curl --with-nghttp2`
-# `brew link curl --force`
-#
 # To run this just run `sh buildbox.sh` in the root directory of Chassis.
 # Please note that due to the size of the base boxes you'll want to have a connection with a good upload speed.
-
-# Check for curl
-if ! [ -x "$(command -v curl)" ]; then
-  echo 'Curl is not installed. Please install curl. `brew install curl --with-nghttp2; brew link curl --force`' >&2
-  exit 1
-fi
-
-# Check for jq
-if ! [ -x "$(command -v jq)" ]; then
-  echo 'jq is not installed. Please install jq: `brew install jq`.' >&2
-  exit 1
-fi
 
 # Store the virtual machines status.
 VM_STATUS=`vagrant status --machine-readable | grep state,running`
@@ -50,24 +33,8 @@ VERSION=1.0.0
 ## Build the base box
 vagrant package --output "chassis-$NOW.box"
 
-# Prompt for a token so we can upload the new box to Vagrant Cloud.
-echo "\033[0;32mWe now need you to copy and paste your Vagrant Cloud authenication token: https://app.vagrantup.com/settings/security\033[0m"
-read -sp 'Token: ' TOKEN
-
-# We need to get an upload path from the Vagrant Cloud API.
-RESPONSE=$(curl --silent --header "Authorization: Bearer $TOKEN" https://app.vagrantup.com/api/v1/box/chassis/chassis/version/$VERSION/provider/virtualbox/upload)
-
-# Requires the jq command.
-UPLOAD_PATH=$(echo "$RESPONSE" | jq -r .upload_path)
-
-if [ null == $UPLOAD_PATH ]; then
-	echo "\n\033[0;31mThere was an issue with your token for Vagrant Cloud.\033[0m"
-	exit 1
-fi
-
 echo "\n\033[0;32mCommencing upload of the new Chassis box to Vagrant Cloud..."
 
-# Do a PUT request to the Vagrant Cloud endpoint and output the progress to the terminal.
-curl $UPLOAD_PATH --request PUT --upload-file "chassis-$NOW.box" --progress-bar | tee /dev/null
+vagrant cloud provider upload chassis/chassis virtualbox $VERSION chassis-$NOW.box
 
 echo "\n\033[0;32mUpload complete!"
