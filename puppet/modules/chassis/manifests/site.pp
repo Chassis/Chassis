045049be26b05e8bff5f72234fe3b72ed165534e
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-20 17:53:05 +1000

    Fix linting

--- puppet/modules/chassis/manifests/site.pp
+++ puppet/modules/chassis/manifests/site.pp
@@ -15,7 +15,7 @@ define chassis::site (
 	$sitename       = 'Chassis Site',
 ) {
 	$extra_hosts = join($hosts, ' ')
-	$server_name = rstrip("${name}")
+	$server_name = rstrip($name)
 	file { $wpdir:
 		ensure => directory
 	}
