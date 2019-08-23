c8bda476f33e30a5d781d002a2bf45b4d4288610
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-23 17:43:54 +1000

    First pass at adding avahi aliases

--- puppet/modules/chassis/manifests/hosts.pp
+++ puppet/modules/chassis/manifests/hosts.pp
@@ -1,39 +1,48 @@
 # Setup the avahi hosts
 class chassis::hosts(
 	$aliases = [],
-	$subdomains = false,
 ) {
 	package { [ 'avahi-daemon', 'python-avahi', 'python-pip' ]:
 		ensure => latest,
 	}
 
-	if ( $subdomains ) {
-
-		file { '/lib/systemd/system/chassis-hosts.service':
-			ensure => 'file',
-			mode   => '0644',
-			source => 'puppet:///modules/chassis/chassis-hosts.service',
-			notify => Exec['systemctl-daemon-reload'],
-		}
-
-		file { '/etc/avahi/aliases':
-			content => template('chassis/aliases.erb'),
-		}
-
-		file { '/vagrant/local-config-hosts.php':
-			source => 'puppet:///modules/chassis/local-config-hosts.php',
-			mode   => '0644',
-		}
-
-		service { 'chassis-hosts':
-			ensure  => running,
-			enable  => true,
-			require => [
-				Package[ 'avahi-daemon' ],
-				Package[ 'python-avahi' ],
-				File[ '/lib/systemd/system/chassis-hosts.service' ],
-				File[ '/etc/avahi/aliases' ],
-			]
-		}
+	exec { 'clone avahi aliases':
+		command => 'git clone https://github.com/hmalphettes/avahi-aliases.git',
+		path    => '/bin',
+		cwd     => '/home/vagrant'
 	}
+
+	exec { 'install avahi aliases':
+		command => 'sudo ./install.sh',
+		path    => '/bin',
+		cwd     => '/home/vagrant',
+		require => Exec['clone avahi aliases']
+	}
+
+	file { '/lib/systemd/system/chassis-hosts.service':
+		ensure => 'file',
+		mode   => '0644',
+		source => 'puppet:///modules/chassis/chassis-hosts.service',
+		notify => Exec['systemctl-daemon-reload'],
+	}
+
+	file { '/etc/avahi/aliases':
+		content => template('chassis/aliases.erb'),
+	}
+
+	file { '/vagrant/local-config-hosts.php':
+		source => 'puppet:///modules/chassis/local-config-hosts.php',
+		mode   => '0644',
+	}
+
+	service { 'chassis-hosts':
+		ensure  => running,
+		enable  => true,
+		require => [
+			Package[ 'avahi-daemon' ],
+			Package[ 'python-avahi' ],
+			File[ '/lib/systemd/system/chassis-hosts.service' ],
+			File[ '/etc/avahi/aliases' ],
+		]
+}
 }
