ed95291de562c2cb03cd6a92d7a46657c57356d1
Bronson Quick <bronson@bronsonquick.com.au>
2023-08-30 15:23:53 +1000

    Remove WP 6.3

--- wp/wp-includes/blocks/file/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '4d04e0384ecd085abe4c');
--- wp/wp-includes/blocks/navigation/view-modal.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6391fb107a84d15c14f4');
